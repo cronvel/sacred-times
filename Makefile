6078902b2f8439d6fd5e29285c21853a74ee7cd8
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2018-09-06 22:41:52 +0200

    Build changed

--- Makefile
+++ Makefile
@@ -4,11 +4,14 @@ BROWSERIFY=browserify
 UGLIFY=uglifyjs
 
 
-browser: browser/SacredTimes.js browser/SacredTimes.min.js
+browser: browser/SacredTimes.js browser/SacredTimes.min.js browser/dom.min.js
 
 browser/SacredTimes.js: lib/SacredTimes.js lib/Event.js
 	${BROWSERIFY} lib/SacredTimes.js -s SacredTimes -o browser/SacredTimes.js
 
 browser/SacredTimes.min.js: browser/SacredTimes.js
 	${UGLIFY} browser/SacredTimes.js -o browser/SacredTimes.min.js -m
+
+browser/dom.min.js: node_modules/dom-kit/browser/dom.min.js
+	cp node_modules/dom-kit/browser/dom.min.js browser/dom.min.js
 
--- browser/SacredTimes.js
+++ browser/SacredTimes.js
@@ -1,4 +1,29 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.SacredTimes = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+/*
+	Sacred Times
+
+	Copyright (c) 2018 Cédric Ronvel
+
+	The MIT License (MIT)
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
 
 "use strict" ;
 
@@ -69,6 +94,31 @@ Event.merge = function( ... eventLists ) {
 
 
 },{"moment":6}],2:[function(require,module,exports){
+/*
+	Sacred Times
+
+	Copyright (c) 2018 Cédric Ronvel
+
+	The MIT License (MIT)
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
 
 "use strict" ;
 
@@ -416,7 +466,7 @@ SacredTimes.prototype.getSacredTimeUpcomingEvents = function( mode = 'legal' ) {
 	Object.keys( sacredTimes ).forEach( name => events.push( new Event( name + 'SacredTime' , sacredTimes[ name ] , undefined , true ) ) ) ;
 
 	events = Event.filterUpToNextMajor( events , startDateTime ) ;
-	
+
 	this.eventsToMode( events , mode ) ;
 
 	return events ;
@@ -442,7 +492,7 @@ SacredTimes.prototype.getCelebrationUpcomingEvents = function( mode = 'legal' ) 
 	Object.keys( celebrations ).forEach( name => events.push( new Event( name + 'CelebrationDay' , celebrations[ name ] , undefined , true ) ) ) ;
 
 	events = Event.filterUpToNextMajor( events , startDateTime ) ;
-	
+
 	this.eventsToMode( events , mode ) ;
 
 	return events ;
