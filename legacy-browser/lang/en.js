b5afb071952e52d82cd94d7bd0ce7ffbee792fa7
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2018-10-24 14:09:22 +0200

    legacy browser fix

--- legacy-browser/lang/en.js
+++ legacy-browser/lang/en.js
@@ -1,40 +1,39 @@
-"use strict";
-
 /*
 	Sacred Times - Copyright (c) 2018 Cédric Ronvel
 	MIT License
 */
 
-LANG.default = LANG.en = {
-	sunrise: "Sunrise",
-	sunset: "Sunset",
+LANG .default =
+LANG.en = {
+	sunrise: "Sunrise" ,
+	sunset: "Sunset" ,
 
-	moonrise: "Moonrise",
-	moonset: "Moonset",
+	moonrise: "Moonrise" ,
+	moonset: "Moonset" ,
+	
+	newMoon: "New Moon" ,
+	fullMoon: "Full Moon" ,
+	firstQuarterMoon: "First Quarter" ,
+	lastQuarterMoon: "Last Quarter" ,
 
-	newMoon: "New Lune",
-	fullMoon: "Full Lune",
-	firstQuarterMoon: "First Quarter",
-	lastQuarterMoon: "Last Quarter",
-
-	springEquinoxSacredTime: "Spring Equinox",
-	summerSolsticeSacredTime: "Summer Solstice",
-	autumnEquinoxSacredTime: "Autumn Equinox",
-	winterSolsticeSacredTime: "Winter Solstice",
-	imbolcSacredTime: "Imbolc",
-	beltaineSacredTime: "Beltaine",
-	lugnasadSacredTime: "Lugnasad",
-	samainSacredTime: "Samain",
-
-	imbolcCelebrationDay: "Imbolc Celebration",
-	ostaraCelebrationDay: "Ostara Celebration",
-	beltaineCelebrationDay: "Beltaine Celebration",
-	lithaCelebrationDay: "Litha Celebration",
-	lugnasadCelebrationDay: "Lugnasad Celebration",
-	mabonCelebrationDay: "Mabon Celebration",
-	samainCelebrationDay: "Samain Celebration",
-	yuleCelebrationDay: "Yule Celebration",
-
-	_fullDateFormat: 'dddd D MMMM YYYY',
+	springEquinoxSacredTime: "Spring Equinox" ,
+	summerSolsticeSacredTime: "Summer Solstice" ,
+	autumnEquinoxSacredTime: "Autumn Equinox" ,
+	winterSolsticeSacredTime: "Winter Solstice" ,
+	imbolcSacredTime: "Imbolc" ,
+	beltaineSacredTime: "Beltaine" ,
+	lugnasadSacredTime: "Lugnasad" ,
+	samainSacredTime: "Samain" ,
+	
+	imbolcCelebrationDay: "Imbolc Celebration" ,
+	ostaraCelebrationDay: "Ostara Celebration" ,
+	beltaineCelebrationDay: "Beltaine Celebration" ,
+	lithaCelebrationDay: "Litha Celebration" ,
+	lugnasadCelebrationDay: "Lugnasad Celebration" ,
+	mabonCelebrationDay: "Mabon Celebration" ,
+	samainCelebrationDay: "Samain Celebration" ,
+	yuleCelebrationDay: "Yule Celebration" ,
+	
+	_fullDateFormat: 'dddd D MMMM YYYY' ,
 	_middleDateTimeFormat: 'ddd D MMM YYYY \\at HH:mm'
-};
+} ;
--- legacy-browser/lang/fr.js
+++ legacy-browser/lang/fr.js
@@ -1,40 +1,38 @@
-"use strict";
-
 /*
 	Sacred Times - Copyright (c) 2018 Cédric Ronvel
 	MIT License
 */
 
-LANG.fr = {
-	sunrise: "Lever de Soleil",
-	sunset: "Coucher de Soleil",
+LANG .fr = {
+	sunrise: "Lever de Soleil" ,
+	sunset: "Coucher de Soleil" ,
+	
+	moonrise: "Lever de Lune" ,
+	moonset: "Coucher de Lune" ,
+	
+	newMoon: "Nouvelle Lune" ,
+	fullMoon: "Pleine Lune" ,
+	firstQuarterMoon: "Premier Quartier" ,
+	lastQuarterMoon: "Dernier Quartier" ,
 
-	moonrise: "Lever de Lune",
-	moonset: "Coucher de Lune",
-
-	newMoon: "Nouvelle Lune",
-	fullMoon: "Pleine Lune",
-	firstQuarterMoon: "Premier Quartier",
-	lastQuarterMoon: "Dernier Quartier",
-
-	springEquinoxSacredTime: "Équinoxe de Printemps",
-	summerSolsticeSacredTime: "Solstice d'Été",
-	autumnEquinoxSacredTime: "Équinoxe d'Automne",
-	winterSolsticeSacredTime: "Solstice d'Hiver",
-	imbolcSacredTime: "Imbolc",
-	beltaineSacredTime: "Beltaine",
-	lugnasadSacredTime: "Lugnasad",
-	samainSacredTime: "Samain",
-
-	imbolcCelebrationDay: "Fête d'Imbolc",
-	ostaraCelebrationDay: "Fête d'Ostara",
-	beltaineCelebrationDay: "Fête de Beltaine",
-	lithaCelebrationDay: "Fête de Litha",
-	lugnasadCelebrationDay: "Fête de Lugnasad",
-	mabonCelebrationDay: "Fête de Mabon",
-	samainCelebrationDay: "Fête de Samain",
-	yuleCelebrationDay: "Fête de Yule",
-
-	_fullDateFormat: 'dddd D MMMM YYYY',
+	springEquinoxSacredTime: "Équinoxe de Printemps" ,
+	summerSolsticeSacredTime: "Solstice d'Été" ,
+	autumnEquinoxSacredTime: "Équinoxe d'Automne" ,
+	winterSolsticeSacredTime: "Solstice d'Hiver" ,
+	imbolcSacredTime: "Imbolc" ,
+	beltaineSacredTime: "Beltaine" ,
+	lugnasadSacredTime: "Lugnasad" ,
+	samainSacredTime: "Samain" ,
+	
+	imbolcCelebrationDay: "Fête d'Imbolc" ,
+	ostaraCelebrationDay: "Fête d'Ostara" ,
+	beltaineCelebrationDay: "Fête de Beltaine" ,
+	lithaCelebrationDay: "Fête de Litha" ,
+	lugnasadCelebrationDay: "Fête de Lugnasad" ,
+	mabonCelebrationDay: "Fête de Mabon" ,
+	samainCelebrationDay: "Fête de Samain" ,
+	yuleCelebrationDay: "Fête de Yule" ,
+	
+	_fullDateFormat: 'dddd D MMMM YYYY' ,
 	_middleDateTimeFormat: 'ddd D MMM YYYY à HH:mm'
-};
+} ;
